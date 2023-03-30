ee669250fdeb7d5d470643a793a92c3f193e859a
Sam Liu <Sam.Liu@hearst.com>
2023-03-30 15:38:00 -0400

    lib/env.ts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const hello = require('./hello');
 const helloJS = require('./hello/hello-js');
 const helloTS = require('./hello/hello-ts');
+const env = require('./lib/env');
 console.log({ hello, helloJS, helloTS });
 hello();
 helloJS();
 helloTS();
+console.log(env());
